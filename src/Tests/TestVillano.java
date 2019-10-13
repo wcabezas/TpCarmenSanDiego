9070b4d8b9204a09fb7457493d4be80ee7746c5f
Shiel000 <51185886+Shiel000@users.noreply.github.com>
2019-10-12 23:25:49 -0300

    se cambio los atribulos del villano ( de publico paso a privado

---
+++ src/Tests/TestVillano.java
@@ -0,0 +1,19 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+class TestVillano {
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- src/Tp/Villano.java
+++ src/Tp/Villano.java
@@ -2,40 +2,28 @@ package Tp;
 import java.util.*;
 
 public class Villano{
-	public String nombre;
-	public String sexo;
-	public List<String>seniasParticulares;
-	public List<String>hobbies;
+	private String nombre;
+	private String sexo;
+	private List<String>seniasParticulares;
+	private List<String>hobbies;
 	private List<Pais>rutaDeescape;
 	
-	public List<Pais> getRutaDeescape() {
-		return rutaDeescape;
-	}
-	public void setRutaDeescape(List<Pais> rutaDeescape) {
-		this.rutaDeescape = rutaDeescape;
-	}
 	public String getNombre() {
 		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
 	}
 	public String getSexo() {
 		return sexo;
 	}
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
 	public List<String> getSeniasParticulares() {
 		return seniasParticulares;
-	}
-	public void setSeniasParticulares(List<String> seniasParticulares) {
-		this.seniasParticulares = seniasParticulares;
 	}
 	public List<String> getHobbies() {
 		return hobbies;
 	}
-	public void setHobbies(List<String> hobbies) {
-		this.hobbies = hobbies;
+	public List<Pais> getRutaDeescape() {
+		return rutaDeescape;
 	}
+
+	
+
 }
